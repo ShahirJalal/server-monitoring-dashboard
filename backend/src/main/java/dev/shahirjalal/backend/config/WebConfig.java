3497b82c07c8c4a1ab4c053c4886aef1633ecff6
Shahir Jalal <shahirjalalhazli@gmail.com>
2026-07-08 14:32:47 +0800

    Allowed origin from all

--- backend/src/main/java/dev/shahirjalal/backend/config/WebConfig.java
+++ backend/src/main/java/dev/shahirjalal/backend/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
